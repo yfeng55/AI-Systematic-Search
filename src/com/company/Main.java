26f6e5da77630c4364898758719ee21a6539e5f4
Evan Feng <evanfeng55@gmail.com>
2016-09-14 00:14:47 -0400

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+
+    private static final String INPUT_FILE = "input.txt";
+
+    private static ArrayList<Number> task_lengths = new ArrayList<>();
+    private static ArrayList<Number> processor_speeds = new ArrayList<>();
+    private static float deadline;
+    private static float target;
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File(INPUT_FILE);
+        readInputFile(file);
+
+
+        System.out.println(task_lengths.toString());
+        System.out.println(processor_speeds.toString());
+        System.out.println(deadline);
+        System.out.println(target);
+    }
+
+
+    // initialize values from input txt file //
+    private static void readInputFile(File f) throws FileNotFoundException {
+
+        Scanner s = new Scanner(f);
+
+        String[] firstline = s.nextLine().split(" +");
+        for(String c : firstline){
+            task_lengths.add(Float.parseFloat(c));
+        }
+
+        String[] secondline = s.nextLine().split(" +");
+        for(String c : secondline){
+            processor_speeds.add(Float.parseFloat(c));
+        }
+
+        deadline = Float.parseFloat(s.next());
+        target = Float.parseFloat(s.next());
+    }
+
+
+}
