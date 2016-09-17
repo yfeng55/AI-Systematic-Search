137378df7a98db0e864fea579214bdf88680e3e9
Evan Feng <evanfeng55@gmail.com>
2016-09-17 16:56:12 -0400

    first

--- src/com/yf833/Main.java
+++ src/com/yf833/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.yf833;
 
 import java.io.File;
 import java.io.FileNotFoundException;
---
+++ src/com/yf833/Node.java
@@ -0,0 +1,14 @@
+package com.yf833;
+
+
+import java.util.ArrayList;
+
+public class Node {
+
+    public ArrayList<Number> assignments;
+
+    public Node(){
+
+    }
+
+}
