da1076df6dad53eb65cb3b29285eae3ba3d9fac2
Evan Feng <evanfeng55@gmail.com>
2016-09-17 19:48:11 -0400

    refactor

--- src/com/yf833/Main.java
+++ src/com/yf833/Main.java
@@ -10,6 +10,8 @@ public class Main {
 
     private static final String INPUT_FILE = "input.txt";
 
+    private static int num_tasks;
+    private static int num_processors;
     private static ArrayList<Number> task_lengths = new ArrayList<>();
     private static ArrayList<Number> processor_speeds = new ArrayList<>();
     private static float deadline;
@@ -22,11 +24,36 @@ public class Main {
         readInputFile(file);
 
 
-        System.out.println(task_lengths.toString());
-        System.out.println(processor_speeds.toString());
-        System.out.println(deadline);
-        System.out.println(target);
+//        System.out.println("TASK LENGTHS: " + task_lengths.toString());
+//        System.out.println("PROCESSOR SPEEDS: " + processor_speeds.toString());
+//        System.out.println("DEADLINE: " + deadline);
+//        System.out.println("TARGET: " + target);
+//        System.out.println("NUM TASKS: " + num_tasks);
+//        System.out.println("NUM PROCESSORS: " + num_processors);
+
+
+
+        
+
+
     }
+
+
+
+
+    //TODO: implement goal function
+    private static boolean isGoal(Node n){
+        return false;
+    }
+
+    //TODO: implement fail function
+    private static boolean isFail(Node n){
+        return false;
+    }
+
+
+
+
 
 
     // initialize values from input txt file //
@@ -46,7 +73,14 @@ public class Main {
 
         deadline = Float.parseFloat(s.next());
         target = Float.parseFloat(s.next());
+
+        num_tasks = task_lengths.size();
+        num_processors = processor_speeds.size();
     }
+
+
+
+
 
 
 }
--- src/com/yf833/Node.java
+++ src/com/yf833/Node.java
@@ -5,10 +5,29 @@ import java.util.ArrayList;
 
 public class Node {
 
-    public ArrayList<Number> assignments;
+    //array of arrays -- index is the processor, inner array is the set of tasks assigned to that processor
+    public ArrayList<ArrayList<Number>> assignments;
 
     public Node(){
 
     }
+
+    //TODO: implement totalTimeTaken()
+    public int totalTimeTaken(){
+        return 0;
+    }
+
+    //TODO: implement totalValue()
+    public int totalValue(){
+        return 0;
+    }
+
+
+
+    // calculate the time it takes to complete a certain task
+    private static float calcTimeTaken(int task_length, int processor_speed){
+        return ((float) task_length) / processor_speed;
+    }
+
 
 }
